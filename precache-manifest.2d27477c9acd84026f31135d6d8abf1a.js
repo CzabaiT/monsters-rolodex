779b36f687b8b742f83c6fee5a257b742589f2a4
Tamás Czabai <czabait@gmail.com>
2020-03-29 12:17:02 +0200

    Updates

---
+++ precache-manifest.2d27477c9acd84026f31135d6d8abf1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669fb865247c89e102ba613b8e72acb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "089515523a04b3be9202",
+    "url": "/static/css/main.22f4d56d.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "089515523a04b3be9202",
+    "url": "/static/js/main.78f77191.chunk.js"
+  },
+  {
+    "revision": "f64cd0e41d72611b1795",
+    "url": "/static/js/runtime-main.ee82bde8.js"
+  }
+]);
