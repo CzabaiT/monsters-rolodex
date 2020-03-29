80658ac8b0d13f7393fe5c6afc2de159194dfcad
Tamás Czabai <czabait@gmail.com>
2020-03-29 12:21:23 +0200

    Updates

---
+++ precache-manifest.d83e7bcde797feb1b9494a1a52bfdb8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2437edc94fb0654d9f62bd2930420db6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2d0df12a58aa52960f0e",
+    "url": "/monsters-rolodex/static/css/main.22f4d56d.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d0df12a58aa52960f0e",
+    "url": "/monsters-rolodex/static/js/main.667c3b36.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
